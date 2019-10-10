07f88d8da5fc0246da9b708b83fc1c491abbad82
Halil Bahar <halil.s.bahar@gmail.com>
2019-10-10 09:23:18 +0200

    Create JUnit test for requesting the server time

---
+++ src/test/java/restprimer/rest/TimeServerResourceIT.java
@@ -0,0 +1,34 @@
+package restprimer.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+
+public class TimeServerResourceIT {
+
+    private Client client;
+    private WebTarget targetUnderTest;
+
+    @Before
+    public void initClient() {
+        this.client = ClientBuilder.newClient();
+        this.targetUnderTest = this.client.target("http://localhost:8080/restprimer/api/time");
+    }
+
+    @Test
+    public void fetchTime() {
+        Response response = this.targetUnderTest.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("Time: "));
+    }
+}
